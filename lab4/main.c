1759e3cc1f03e28c2b9c87757ebb947fe3d5c56e
hayden <farand@ualberta.ca>
2026-03-19 15:54:21 -0600

    parallelized

--- lab4/main.c
+++ lab4/main.c
@@ -23,13 +23,11 @@ int main (int argc, char* argv[]){
     int iterationcount;
     double start, end;
     FILE *ip;
-    /* INSTANTIATE MORE VARIABLES IF NECESSARY */
     int numtasks, rank;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    
     // load data 
     if ((ip = fopen("data_input_meta","r")) == NULL) {
         printf("Error opening the data_input_meta file.\n");
@@ -42,32 +40,59 @@ int main (int argc, char* argv[]){
     // initialize variables
     r = malloc(nodecount * sizeof(double));
     r_pre = malloc(nodecount * sizeof(double));
-    
+
     iterationcount = 0;
-    for ( i = 0; i < nodecount; ++i)
+    for (i = 0; i < nodecount; ++i)
         r[i] = 1.0 / nodecount;
     /* INITIALIZE MORE VARIABLES IF NECESSARY */
+    
+    int local_start = rank * (nodecount / numtasks) + (rank < nodecount % numtasks ? rank : nodecount % numtasks);
+    int local_end   = local_start + nodecount / numtasks + (rank < nodecount % numtasks ? 1 : 0);
 
+    int *counts = malloc(numtasks * sizeof(int));
+    int *displs = malloc(numtasks * sizeof(int));
+    for (i = 0; i < numtasks; ++i) {
+        counts[i] = nodecount / numtasks + (i < nodecount % numtasks ? 1 : 0);
+        displs[i] = i * (nodecount / numtasks) + (i < nodecount % numtasks ? i : nodecount % numtasks);
+    }
+    double *r_local = malloc((local_end - local_start) * sizeof(double));
+
+    double local_diff2, local_norm2, gdiff2, gnorm2;
 
     GET_TIME(start);
     // core calculation
-    do{
+    do {
         ++iterationcount;
-        /* IMPLEMENT ITERATIVE UPDATE */
-        vec_cp(r, r_pre, nodecount);
-
-        for (i = 0; i < nodecount; ++i){
-            r[i] = 0.0;
+        for (i = local_start; i < local_end; ++i) r_pre[i] = r[i];
+        for (i = local_start; i < local_end; ++i) r[i] = 0.0;
+        for (i = local_start; i < local_end; ++i){
             for (j = 0; j < nodehead[i].num_in_links; ++j){
                 r[i] += r_pre[nodehead[i].inlinks[j]] / nodehead[nodehead[i].inlinks[j]].num_out_links;
             }
             r[i] = DAMPING_FACTOR * r[i] + (1 - DAMPING_FACTOR) / nodecount;
         }
 
-    }while(rel_error(r, r_pre, nodecount) >= EPSILON);
+        for (i = local_start; i < local_end; ++i) r_local[i - local_start] = r[i];
+        MPI_Allgatherv(r_local, local_end - local_start, MPI_DOUBLE,
+                       r, counts, displs, MPI_DOUBLE, MPI_COMM_WORLD);
+
+        local_diff2 = 0.0; local_norm2 = 0.0;
+        for (i = local_start; i < local_end; ++i) {
+            double d = r[i] - r_pre[i];
+            local_diff2 += d * d;
+            local_norm2 += r_pre[i] * r_pre[i];
+        }
+        MPI_Allreduce(&local_diff2, &gdiff2, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+        MPI_Allreduce(&local_norm2, &gnorm2, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+    } while(sqrt(gdiff2 / gnorm2) >= EPSILON);
     GET_TIME(end);
 
-    Lab4_saveoutput(r, nodecount, end - start);
+    free(counts); free(displs); free(r_local);
+
+    if (rank == 0) {
+        Lab4_saveoutput(r, nodecount, end - start);
+        printf("Time: %f\n", end - start);
+    }
 
     // post processing
     node_destroy(nodehead, nodecount);
